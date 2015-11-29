9ed5fc145342867d737003b6adfb0cae0fb9d2a7
randallkanna <randallkanna@gmail.com>
2015-11-28 16:13:14 -0800

    modifying master detail template

--- BlogReader/MasterViewController.h
+++ BlogReader/MasterViewController.h
@@ -10,6 +10,7 @@
 
 @interface MasterViewController : UITableViewController
 
+@property (strong, nonatomic) NSArray *titlesArray;
 
 @end
 
--- BlogReader/MasterViewController.m
+++ BlogReader/MasterViewController.m
@@ -9,10 +9,10 @@
 #import "MasterViewController.h"
 #import "DetailViewController.h"
 
-@interface MasterViewController ()
-
-@property NSMutableArray *objects;
-@end
+//@interface MasterViewController ()
+//
+//@property NSMutableArray *objects;
+//@end
 
 @implementation MasterViewController
 
@@ -22,11 +22,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    self.titlesArray = [NSArray arrayWithObjects:@"Getting started with WordPress",
+                        @"Whitespace in Web Design: What It Is and Why You Should Use It",
+                        @"Adaptive Images and Responsive SVGs - Treehouse Show Episode 15",
+                        @"Productivity is About Constraints and Concentration",
+                        @"A Guide to Becoming the Smartest Developer on the Planet",
+                        @"Teacher Spotlight: Zac Gordon",
+                        @"Do You Love What You Do?",
+                        @"Applying Normalize.css Reset - Quick Tip",
+                        @"How I Wrote a Book in 3 Days",
+                        @"Responsive Techniques, JavaScript MVC Frameworks, Firefox 16 | Treehouse Show Episode 14", nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,23 +39,14 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender {
-    if (!self.objects) {
-        self.objects = [[NSMutableArray alloc] init];
-    }
-    [self.objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-}
-
 #pragma mark - Segues
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if ([[segue identifier] isEqualToString:@"showDetail"]) {
-        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = self.objects[indexPath.row];
-        [[segue destinationViewController] setDetailItem:object];
-    }
+//    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+//        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+//        NSDate *object = self.objects[indexPath.row];
+//        [[segue destinationViewController] setDetailItem:object];
+//    }
 }
 
 #pragma mark - Table View
@@ -60,29 +56,20 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.objects.count;
+    return [self.titlesArray count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    NSDate *object = self.objects[indexPath.row];
-    cell.textLabel.text = [object description];
+    NSString *object = self.titlesArray[indexPath.row];
+    cell.textLabel.text = object;
     return cell;
 }
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
-}
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [self.objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
 }
 
 @end
